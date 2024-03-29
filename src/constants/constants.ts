a5dedc95200b78d905077977c61970674287ec1b
minhpng <nguyenminh.236@gmail.com>
2024-03-29 15:36:16 +0700

    fix map link

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -2,4 +2,5 @@ export const HOTLINE = `0964536819`
 export const ZALO_LINK = `https://zalo.me/0964536819`
 export const HOTLINE_FORMAT = `096.453.6819`
 export const ADDRESS = "1446 Phạm Thế Hiển, P5, Q8"
-export const ADDRESS_MAP_LINK = "1446 Phạm Thế Hiển, P5, Q8"
+export const ADDRESS_MAP_LINK =
+	"https://maps.app.goo.gl/DcdR37tMoV3d9AZcA?g_st=ic"
